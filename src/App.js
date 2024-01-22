8b0e39414a1b8a87bf7c9e9b5ce6abbf241b48c7
MANISH KUMAR PRAJAPATI <71719780+ManishkPrajapati@users.noreply.github.com>
2024-01-22 23:13:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import IDcard from "./IDcard";
 
 function App() {
 
-  let arr = [10,20,30,40,50]
+  let arr = [10,20,30,40,50,74,93,84,363,939]
   let obj = {name:"Manish", dob: "29/05/2001", email:"matt353730@gmail.com"}
   return (
     <>
